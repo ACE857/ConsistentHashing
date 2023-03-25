1c4459b0c24039bc0608cc06047c2bc175f89c5f
ACE857 <srjbsht857@gmail.com>
2023-03-25 19:37:57 +0530

    Added minor comments

--- Java/src/ConsistentHashing.java
+++ Java/src/ConsistentHashing.java
@@ -1,3 +1,14 @@
 public class ConsistentHashing {
-    
+    /**
+     * 1. HashRing
+     * 2. ServerMap -> server to virtual node map
+     * 3. key distribution map -> keys range to server map
+     *
+     * methods
+     * init
+     * add server
+     * remove server
+     * mapKeys -> maps keys to servers
+     * getServer(key)
+     */
 }
